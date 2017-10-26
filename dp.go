b82ae3949561c2eade8c9b47e17aaa18b964eb70
Titus Tienaah <titus.intdxdt@gmail.com>
2017-10-26 14:45:27 -0300

    simple

--- dp.go
+++ dp.go
@@ -1,23 +1,27 @@
 package offset
 
 import (
-	"simplex/rng"
 	"github.com/intdxdt/geom"
 )
 
 //euclidean offset distance from dp - anchor line [i, j] to maximum
 //vertex at i < k <= j - not maximum offset is may not  be perpendicular
-func MaxOffset(coordinates []*geom.Point, rng *rng.Range) (int, float64) {
-	var seg = geom.NewSegment(coordinates[rng.I()], coordinates[rng.J()])
-	var index, offset = rng.J(), 0.0
+//note : coordinates of node from begin 0 to n-1 find k
+func MaxOffset(coordinates []*geom.Point) (int, float64) {
+	var n = len(coordinates) - 1
+	var index, offset = n, 0.0
+	if n <= 1 {
+		return index, offset
+	}
 
-	if rng.Size() > 1 {
-		for _, k := range rng.ExclusiveStride(1) {
-			var dist = seg.DistanceToPoint(coordinates[k])
-			if dist >= offset {
-				index, offset = k, dist
-			}
+	var dist float64
+	var seg = geom.NewSegment(coordinates[0], coordinates[n])
+	for k := 0; k <= n; k++ {
+		dist = seg.DistanceToPoint(coordinates[k])
+		if dist >= offset {
+			index, offset = k, dist
 		}
 	}
+
 	return index, offset
 }
--- sed.go
+++ sed.go
@@ -1,28 +1,30 @@
 package offset
 
 import (
-	"simplex/rng"
 	"github.com/intdxdt/vect"
 	"github.com/intdxdt/geom"
 )
 
 //computes Synchronized Euclidean Distance
-func MaxSEDOffset(coordinates []*geom.Point, rng *rng.Range) (int, float64) {
+func MaxSEDOffset(coordinates []*geom.Point) (int, float64) {
 	var t = 2
-	var index, offset = rng.J(), 0.0
-	var a, b = coordinates[rng.I()], coordinates[rng.J()]
+	var n = len(coordinates) - 1
+	var index, offset = n, 0.0
+	if n <= 1 {
+		return index, offset
+	}
+
+	var a, b = coordinates[0], coordinates[n]
 	var opts = &vect.Options{A: a, B: b, At: &a[t], Bt: &b[t]}
-	var segVect = vect.NewVect(opts)
-	var sedVect *vect.Vect
 	var dist float64
 
-	if rng.Size() > 1 {
-		for _, k := range rng.ExclusiveStride(1) {
-			sedVect = segVect.SEDVector(coordinates[k], coordinates[k][t])
-			dist = sedVect.Magnitude()
-			if dist >= offset {
-				index, offset = k, dist
-			}
+	var pt *geom.Point
+	var vec = vect.NewVect(opts)
+	for k := 0; k <= n; k++ {
+		pt = coordinates[k]
+		dist = vec.SEDVector(pt, pt[geom.Z]).Magnitude()
+		if dist >= offset {
+			index, offset = k, dist
 		}
 	}
 	return index, offset
