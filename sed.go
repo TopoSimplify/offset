a1ce59184640d1ecff0b4f11e24af4d4d0d877f0
Titus Tienaah <titus.intdxdt@gmail.com>
2018-08-04 22:24:34 -0300

    fix coords

--- sed.go
+++ sed.go
@@ -8,9 +8,9 @@ import (
 
 //@formatter:off
 //computes Synchronized Euclidean Distance
-func MaxSEDOffset(coordinates []geom.Point) (int, float64) {
+func MaxSEDOffset(coordinates geom.Coords) (int, float64) {
 
-	var n = len(coordinates) - 1
+	var n = coordinates.Len() - 1
 	var index, offset = n, 0.0
 	if n <= 1 {
 		return index, offset
@@ -19,18 +19,18 @@ func MaxSEDOffset(coordinates []geom.Point) (int, float64) {
 	var pt *geom.Point
 	var dist, m, ptx, pty, ptt, px, py float64
 
-	var a, b = coordinates[0], coordinates[n]
+	var a, b = coordinates.Pt(0), coordinates.Pt(n)
 	var ax, ay  = a[geom.X], a[geom.Y]
 	var at , bt = a[geom.Z], b[geom.Z]
 
-	var v = vect.NewVector(a, b)
+	var v = vect.NewVector(*a, *b)
 	var mij = v.Magnitude()
 	var fb = direction(v[geom.X], v[geom.Y])
 	var vx, vy = math.Cos(fb), math.Sin(fb)
 	var dt = bt - at
 
 	for k := 1; k < n; k++ { //exclusive range between 0 < k < n
-		pt = &coordinates[k]
+		pt = coordinates.Pt(k)
 		ptx, pty, ptt = pt[geom.X], pt[geom.Y], pt[geom.Z]
 
 		m = (mij / dt) * (ptt - at)
